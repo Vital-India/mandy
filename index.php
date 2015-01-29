ed810bd154cd5f785eea26a91070176ffe01a65a
Rathinasamy <rathina.vitaldesign@gmail.com>
2015-01-29 19:30:36 +0530

    Removed htaccess and added index.php

---
+++ index.php
@@ -0,0 +1,58 @@
+<html>
+    <head>
+        <title>Mandy Technologies Private Limited</title>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+        <link rel="icon" href="favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Candal">
+            <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+        <style>
+          body {
+            font-family: 'Candal', serif;
+            font-size: 40px;
+            margin: 0;
+            padding: 0;
+          }
+          .topborder {
+            font-family: 'Oswald', serif;
+            background: #FD5708;
+            height: 22px;
+            width:100%;
+            font-size: 14px;
+            color: #fff;
+            text-align: center;
+          }
+          .container{
+            margin: 70px auto;
+            width:999px;
+            text-align: center;
+            color: #2A2A2A;
+          }
+          a {
+            color: #666666;
+            font-size: 30px;
+            text-decoration: none;  
+          }
+          a:hover{
+            background: #FD5708;
+            color: #fff;
+          }
+        </style>
+    </head>
+    <body>
+        <div class="topborder">{a web development company}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;#wordpress&nbsp;&nbsp;&nbsp;&nbsp;#magento</div>
+        <div class="container">
+            <img src="logo.jpg" alt="Mandy Technologies Private Limited" title="Mandy Technologies Private Limited" /> <br /><br />
+            <p>Website is under development. In the mean, you shall contact us here <br /><a href="mailto:info@mandytechnologies.com">info@mandytechnologies.com</a></p>
+        </div>
+        
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+          
+            ga('create', 'UA-58431261-1', 'auto');
+            ga('send', 'pageview');
+        </script>
+    </body>
+</html>
